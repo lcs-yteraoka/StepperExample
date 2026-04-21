514ca950e9384c340cc633beb9264ab2f357994a
Yahiro Teraoka <yteraoka@lcs.on.ca>
2026-04-21 08:58:14 -0400

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Yahiro Teraoka on 2026-04-21.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
